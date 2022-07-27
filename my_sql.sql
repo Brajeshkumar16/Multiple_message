d737f8bf09335f7c7c31f85d297e94cc0d8f50f7
Brajeshkumar16 <109973687+Brajeshkumar16@users.noreply.github.com>
2022-07-28 00:03:59 +0530

    Add files via upload

---
+++ my_sql.sql
@@ -0,0 +1,11 @@
+show databases;
+create database sailes;
+use sailes;
+create table employees(EID INT NOT NULL, NAME VARCHAR(20), NUMBER VARCHAR(10), MESSAGE VARCHAR(500));
+SHOW TABLES;
+INSERT INTO EMPLOYEES VALUES(1,'Harshit','62034*****','Hii  ');
+INSERT INTO EMPLOYEES VALUES(1,'ankit','94703*****','Hii bhaiya  ');
+
+
+drop table employees
+SELECT * FROM EMPLOYEES
